dea06819acfa2295c6ca06fb99da18148c303667
rishichaulagain27-sys <rishi.chaulagain27@gmail.com>
2026-01-15 15:13:12 +0930

    adding decryption code

--- Encryption Question/code.py
+++ Encryption Question/code.py
@@ -33,4 +33,38 @@ def encrypt_char(shift1, shift2):
 
     with open("encrypted.txt", "w") as file:
         file.write(Evalue)
+#Decryption function
+def decrypt_char(shift1, shift2):
+    l1 = "abcdefghijklm"
+    l2 = "nopqrstuvwxyz"
+    u1 = "ABCDEFGHIJKLM"
+    u2 = "NOPQRSTUVWXYZ"
+    Dvalue = ""
 
+    with open("encrypted.txt", "r") as file:
+        txt = file.read()
+
+    for c in txt:
+        if c.islower():
+            if c in l1:
+                idx = l1.index(c)
+                new_idx = (idx - (shift1 * shift2)) % 13
+                Dvalue += l1[new_idx]
+            elif c in l2:
+                idx = l2.index(c)
+                new_idx = (idx + (shift1 + shift2)) % 13
+                Dvalue += l2[new_idx]
+        elif c.isupper():
+            if c in u1:
+                idx = u1.index(c)
+                new_idx = (idx + shift1) % 13
+                Dvalue += u1[new_idx]
+            elif c in u2:
+                idx = u2.index(c)
+                new_idx = (idx - (shift2 ** 2)) % 13
+                Dvalue += u2[new_idx]
+        else:
+            Dvalue += c
+
+    with open("decrypted.txt", "w") as file:
+        file.write(Dvalue)
